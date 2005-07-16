b067dd328b0e0293bbd11f41141cf03c41a2a5cb
Andy Scukanec <ags@cs.cornell.edu>
2005-07-16 08:04:27 +0000

    *** empty log message ***

--- src/fedora/services/sipcreator/Constants.java
+++ src/fedora/services/sipcreator/Constants.java
@@ -1,11 +1,9 @@
 package fedora.services.sipcreator;
 
-import java.io.File;
-
 public interface Constants {
 
-    public static final String CONFIG_FILE_NAME = "config" + File.separator + "sipcreator.properties";
-    public static final String IMAGE_DIR_NAME = "images" + File.separator;
+    public static final String CONFIG_FILE_NAME = "config/sipcreator.properties";
+    public static final String IMAGE_DIR_NAME = "images/";
     
     public static final String METS_NS = "http://www.loc.gov/METS/";
     public static final String OAIDC_NS = "http://www.openarchives.org/OAI/2.0/oai_dc/";
